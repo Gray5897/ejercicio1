45be2f8d022f3e915e17df893318f9cd8baa51d9
Gustavo Brand <gustavo@raqoon-brand.com>
2024-01-27 10:19:39 -0500

    Se realiza funcion que permite crear los targets

--- index.php
+++ index.php
@@ -1,5 +1,15 @@
 <?php
-
 include "views/layouts/header.php";
 include "views/home.php";
 include "views/layouts/footer.php";
+
+/*try {
+	$database = new Database();
+	$database->db->query('INSERT INTO targets(name) VALUES("Juan")');
+	$targets = $database->db->query('SELECT * from targets');
+	foreach ($targets as $target) {
+		print_r($target);
+	}
+} catch (PDOException $e) {
+	print "¡Error!: " . $e->getMessage() . "<br/>";
+}*/
---
+++ views/targets/create.php
@@ -0,0 +1,28 @@
+<?php
+
+require_once '../../vendor/autoload.php';
+
+use App\Controllers\TargetController;
+
+include "../layouts/header.php";
+
+
+try {
+	if (isset($_POST['btn-save'])) {
+		$target = new TargetController();
+		$target->saveTarget($_POST['name']);
+	}
+} catch (PDOException $e) {
+	print "¡Error!: " . $e->getMessage() . "<br />";
+}
+?>
+
+<form method="POST">
+	<input type="text" placeholder="Escriba su nombre" name="name">
+	<button type="submit" name="btn-save">Guardar</button>
+</form>
+
+
+<?php
+include "../layouts/footer.php";
+?>
