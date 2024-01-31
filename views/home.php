7b062bc0c3c4fe8b3e700115647613dbde3d4990
Gray5897 <66230845+Gray5897@users.noreply.github.com>
2024-01-30 22:55:03 -0500

    Revert "Feature/update target"

--- views/home.php
+++ views/home.php
@@ -3,38 +3,18 @@
 $data = file_get_contents("public/docs/example.json");
 $targets = json_decode($data, true);
 
-require_once 'vendor/autoload.php';
-
-use App\Controllers\TargetController;
-
-$targetController = new TargetController();
-$targets = $targetController->getTargets();
-
 ?>
 
-<div class="btn-create-target">
-	<button onClick="openCreateView()">Crear target</button>
-</div>
 <div class="container">
 	<?php
 	foreach ($targets as $target) {
 		echo '<div class="target" id"' . $target['id'] . '">
                 <img class= "imgs" src="' . $target['image'] . '" alt="">
                 <div class="titarget">
-                    ' . $target['name'] . '
+                    ' . $target['title'] . '
                 </div>
                 <a href="controllers/detalleimage.php" class="detalle" id="' . $target['id'] . '">Ver detalle</a>
-                <button onclick="OpenUpdateTarget(' . $target['id'] . ')" class="btn-update" id="' . $target['id'] . '">Actualizar</button>
             </div>';
 	}
 	?>
 </div>
-
-<script>
-	function openCreateView() {
-		window.location.replace('/ejercicio1/views/targets/create.php');
-	}
-    function OpenUpdateTarget(id) {
-        window.location.replace('/ejercicio1/views/targets/update.php?id='+id);
-    }
-</script>
