be5c9484d58182cc71c930623425e7b16b6dcbd6
alejandro cubides <123jefersaenz@gmail.com>
2024-01-30 21:55:34 -0500

    se realzia la creacion de un boto de retroceso y se corrige un error.

--- views/targets/update.php
+++ views/targets/update.php
@@ -6,39 +6,39 @@ use App\Controllers\TargetController;
 
 include "../layouts/header.php";
 
-try {	
-	if (isset($_POST['btn-cancel'])) {
-		header('Location: http://localhost/ejercicio1');
-	}
+try {
+  if (isset($_POST['btn-cancel'])) {
+    header('Location: http://localhost/ejercicio1');
+  }
 } catch (PDOException $e) {
-print "¡Error!: " . $e->getMessage() . "<br />";
-}
-
-try {	
-		$target = new TargetController();
-		$onetarget=$target->getOneTarget($_GET['id']);
-} catch (PDOException $e) {
-	print "¡Error!: " . $e->getMessage() . "<br />";
+  print "¡Error!: " . $e->getMessage() . "<br />";
 }
 
 try {
-	if (isset($_POST['btn-update'])) {
-		if(empty($_POST['id']) or empty($_POST['name']) or empty($_POST['image'])){
-			echo "Debe llenar todos los campos, no se permiten valores vacios";			
-		}else{			
-			$saveupdatetarget = new TargetController();
-			$saveupdatetarget->saveUpdateTarget($_POST['id'], $_POST['name'], $_POST['image']);
-		}
-	}
+  $target = new TargetController();
+  $onetarget = $target->getOneTarget($_GET['id']);
 } catch (PDOException $e) {
-	print "¡Error!: " . $e->getMessage() . "<br />";
+  print "¡Error!: " . $e->getMessage() . "<br />";
 }
 
-    echo '<div>
+try {
+  if (isset($_POST['btn-update'])) {
+    if (empty($_POST['id']) or empty($_POST['name']) or empty($_POST['image'])) {
+      echo "Debe llenar todos los campos, no se permiten valores vacios";
+    } else {
+      $saveupdatetarget = new TargetController();
+      $saveupdatetarget->saveUpdateTarget($_POST['id'], $_POST['name'], $_POST['image']);
+    }
+  }
+} catch (PDOException $e) {
+  print "¡Error!: " . $e->getMessage() . "<br />";
+}
+
+echo '<div>
         <form method="POST">
-            Id de Imagen<input type="number" value="'.$onetarget['id'].'" name="id" ><br>
-            Nombre de Imagen<input type="text" value="'.$onetarget['name'].'" name="name"><br>
-            Link de Imagen<input type="text" value="'.$onetarget['image'].'" name="image"><br>
+            Id de Imagen<input type="number" value="' . $onetarget['id'] . '" name="id" ><br>
+            Nombre de Imagen<input type="text" value="' . $onetarget['name'] . '" name="name"><br>
+            Link de Imagen<input type="text" value="' . $onetarget['image'] . '" name="image"><br>
             <button name="btn-update">Actualizar</button><br>
             <button name="btn-cancel">Cancelar</button><br>
         </form>
