6729fb65a54a1d200fd8a264ea22259a977815ac
alejandro cubides <123jefersaenz@gmail.com>
2024-01-30 23:26:37 -0500

    Se realiza la insercion de un boton en cada target el que permite consultar la informacion del target y eliminarla si asi lo solicitan o cancelar la accion y volver al inicio

--- app/Controllers/TargetController.php
+++ app/Controllers/TargetController.php
@@ -8,56 +8,69 @@ use PDO;
 
 class TargetController
 {
-	public function __construct()
-	{
-	}
+  public function __construct()
+  {
+  }
 
-	public function saveTarget($name)
-	{
-		try {
-			$database = new Database();
-			$database->db->query('INSERT INTO targets(name) VALUES("' . $name . '")');
-			header('Location: http://localhost/ejercicio1');
-		} catch (Exception $e) {
-			echo $e->getMessage();
-		}
-	}
+  public function saveTarget($name)
+  {
+    try {
+      $database = new Database();
+      $database->db->query('INSERT INTO targets(name) VALUES("' . $name . '")');
+      header('Location: http://localhost/ejercicio1');
+    } catch (Exception $e) {
+      echo $e->getMessage();
+    }
+  }
 
-	public function getTargets()
-	{
-		try {
-			$database = new Database();
-			$targets = $database->db->query('SELECT * FROM targets');
-			return $targets;
-		} catch (Exception $e) {
-			return $e->getMessage();
-		}
-	}
+  public function getTargets()
+  {
+    try {
+      $database = new Database();
+      $targets = $database->db->query('SELECT * FROM targets');
+      return $targets;
+    } catch (Exception $e) {
+      return $e->getMessage();
+    }
+  }
 
-	public function getOneTarget($id)
-	{
-		try {
-			$database = new Database($id);
-			$target = $database->db->prepare('SELECT * FROM targets WHERE id = :id');
-			$target->execute(['id'=>$id]);
-			return $target->fetch(PDO::FETCH_ASSOC);
-		} catch (Exception $e) {
-			return $e->getMessage();
-		}
-	}
+  public function getOneTarget($id)
+  {
+    try {
+      $database = new Database($id);
+      $target = $database->db->prepare('SELECT * FROM targets WHERE id = :id');
+      $target->execute(['id' => $id]);
+      return $target->fetch(PDO::FETCH_ASSOC);
+    } catch (Exception $e) {
+      return $e->getMessage();
+    }
+  }
 
-	public function saveUpdateTarget($id, $name, $image)
-	{
-		try {
-			$database = new Database();
-			$saveupdatetarget = $database->db->prepare('UPDATE targets SET name = :name, image = :image WHERE id = :id');
-			$saveupdatetarget->bindParam(':name',$name);
-			$saveupdatetarget->bindParam(':image',$image);
-			$saveupdatetarget->bindParam(':id', $id);
-			$saveupdatetarget->execute();
-			header('Location: http://localhost/ejercicio1');
-		} catch (Exception $e) {
-			echo $e->getMessage();
-		}
-	}
+  public function saveUpdateTarget($id, $name, $image)
+  {
+    try {
+      $database = new Database();
+      $saveupdatetarget = $database->db->prepare('UPDATE targets SET name = :name, image = :image WHERE id = :id');
+      $saveupdatetarget->bindParam(':name', $name);
+      $saveupdatetarget->bindParam(':image', $image);
+      $saveupdatetarget->bindParam(':id', $id);
+      $saveupdatetarget->execute();
+      header('Location: http://localhost/ejercicio1');
+    } catch (Exception $e) {
+      echo $e->getMessage();
+    }
+  }
+
+  public function deleteTarget($id)
+  {
+    try {
+      $database = new Database();
+      $deletetarget = $database->db->prepare('DELETE FROM targets WHERE id = :id');
+      $deletetarget->bindParam(':id', $id);
+      $deletetarget->execute();
+      header('Location: http://localhost/ejercicio1');
+    } catch (Exception $e) {
+      echo $e->getMessage();
+    }
+  }
 }
--- views/home.php
+++ views/home.php
@@ -13,28 +13,34 @@ $targets = $targetController->getTargets();
 ?>
 
 <div class="btn-create-target">
-	<button onClick="openCreateView()">Crear target</button>
+  <button onClick="openCreateView()">Crear target</button>
 </div>
 <div class="container">
-	<?php
-	foreach ($targets as $target) {
-		echo '<div class="target" id"' . $target['id'] . '">
+  <?php
+  foreach ($targets as $target) {
+    echo '<div class="target" id"' . $target['id'] . '">
                 <img class= "imgs" src="' . $target['image'] . '" alt="">
                 <div class="titarget">
                     ' . $target['name'] . '
                 </div>
                 <a href="controllers/detalleimage.php" class="detalle" id="' . $target['id'] . '">Ver detalle</a>
                 <button onclick="OpenUpdateTarget(' . $target['id'] . ')" class="btn-update" id="' . $target['id'] . '">Actualizar</button>
+                <button onclick="OpenDeleteTarget(' . $target['id'] . ')" class="btn-delete" id="' . $target['id'] . '">Eliminar</button>
             </div>';
-	}
-	?>
+  }
+  ?>
 </div>
 
 <script>
-	function openCreateView() {
-		window.location.replace('/ejercicio1/views/targets/create.php');
-	}
-    function OpenUpdateTarget(id) {
-        window.location.replace('/ejercicio1/views/targets/update.php?id='+id);
-    }
+  function openCreateView() {
+    window.location.replace('/ejercicio1/views/targets/create.php');
+  }
+
+  function OpenUpdateTarget(id) {
+    window.location.replace('/ejercicio1/views/targets/update.php?id=' + id);
+  }
+
+  function OpenDeleteTarget(id) {
+    window.location.replace('/ejercicio1/views/targets/delete.php?id=' + id);
+  }
 </script>
